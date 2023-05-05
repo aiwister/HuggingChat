4cf71401d215af0b7a5b89b871f75bfc0a2ead8a
nuller <95852520+aiwister@users.noreply.github.com>
2023-05-05 17:36:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import requests
 import json
 
+
 class HuggingChat:
+
     def __init__(self):
-        self._BASE:str="https://huggingface.co/chat/conversation/"
-        self._ID:str=self._get_id()
-        self.json={
-            "inputs": "",
+        self._BASE: str = "https://huggingface.co/chat/conversation/"
+        self.get_id_json = {"model": "OpenAssistant/oasst-sft-6-llama-30b-xor"}
+        self.json = {
+            "inputs": "hello",
             "parameters": {
                 "temperature": 0.9,
+                "truncate": 1000,
+                "max_new_tokens": 1024,
+                "stop": ["</s>"],
                 "top_p": 0.95,
                 "repetition_penalty": 1.2,
                 "top_k": 50,
-                "truncate": 1024,
-                "watermark": False,
-                "max_new_tokens": 1024,
-                "stop": ["<|endoftext|>"],
                 "return_full_text": False
             },
             "stream": False,
             "options": {
+                "id": "68e2cf66-88d9-4aa0-817c-a8a03040d531",
+                "is_retry": False,
                 "use_cache": False
             }
         }
-        self._session=requests.Session()
-    
+        self._session = requests.Session()
+        self._ID: str = self._get_id()
+
     def _get_id(self):
-        resp=requests.post(self._BASE).content
+        resp = self._session.post(self._BASE, json=self.get_id_json).content
         return json.loads(resp)["conversationId"]
 
-    def ask(self,prompt):
-        self.json["inputs"]=prompt
-        return self._session.post(self._BASE+self._ID,json=self.json).text
+    def ask(self, prompt):
+        self.json["inputs"] = prompt
+        return self._session.post(self._BASE + self._ID, json=self.json).text
 
-chat=HuggingChat()
-print(chat.ask("Hello"))
-        
+
+chat = HuggingChat()
+print(chat.ask("hello"))
